ad68c6a3ff75b9c661f16c76f1fc54cef9b9aa90
SangHyunGil <zizon5941@gmail.com>
2021-12-07 18:08:43 +0900

    StudyFindResponseDto -> 스터디원들 및 스터디 게시판 정보 추가

--- src/main/java/project/SangHyun/domain/repository/StudyCustomRepository.java
+++ src/main/java/project/SangHyun/domain/repository/StudyCustomRepository.java
@@ -1,9 +1,10 @@
 package project.SangHyun.domain.repository;
 
+import project.SangHyun.domain.entity.Study;
 import project.SangHyun.dto.response.StudyFindResponseDto;
 
 import java.util.List;
 
 public interface StudyCustomRepository {
-    StudyFindResponseDto findStudyById(Long studyId);
+    Study findStudyById(Long studyId);
 }
