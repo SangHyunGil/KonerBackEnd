99f18208230297929e898637a9affd730ce8e07b
SangHyunGil <zizon5941@gmail.com>
2022-01-10 17:13:55 +0900

    feat: 쪽지 레포지토리 개발

---
+++ src/main/java/project/SangHyun/message/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package project.SangHyun.message.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.SangHyun.message.domain.Message;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
