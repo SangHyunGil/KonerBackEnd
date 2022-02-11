d9d865b1eb493f2f1f48c1cd15c129ee04b13430
SangHyunGil <zizon5941@gmail.com>
2022-02-11 21:38:50 +0900

    refactor: Member Controller Dto 계속 쓰던 것 Service Layer Dto를 생성하여 분리(Service <-> Controller 결합 제거)

--- src/test/java/project/SangHyun/study/study/controller/unit/StudyControllerUnitTest.java
+++ src/test/java/project/SangHyun/study/study/controller/unit/StudyControllerUnitTest.java
@@ -36,7 +36,6 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
